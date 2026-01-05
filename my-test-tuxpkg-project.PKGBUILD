aa0866a093acc04dd0b66d4ec869fc8dfebbf1ae
Anders Roxell <anders.roxell@linaro.org>
2026-01-05 20:23:01 +0100

    Release 0.13.0
    Signed-off-by: Anders Roxell <anders.roxell@linaro.org>

--- my-test-tuxpkg-project.PKGBUILD
+++ my-test-tuxpkg-project.PKGBUILD
@@ -1,5 +1,5 @@
 pkgname=my-test-tuxpkg-project
-pkgver=0.12.0
+pkgver=0.13.0
 pkgrel=1
 pkgdesc='FIXME'
 url='FIXME'
--- my_test_tuxpkg_project/__init__.py
+++ my_test_tuxpkg_project/__init__.py
@@ -2,7 +2,7 @@
 my-test-tuxpkg-project is ...
 """
 
-__version__ = "0.12.0"
+__version__ = "0.13.0"
 
 
 def main():
